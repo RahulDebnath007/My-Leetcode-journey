add708b064d8c04b71be1322954b3aad1415e994
Rahul Debnath <155256189+RahulDebnath007@users.noreply.github.com>
2026-08-31 13:09:40 +0530

    Time: 7 ms (99.75%), Space: 13.8 MB (71.67%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,18 +1,27 @@
 class Solution(object):
     def minPathSum(self, grid):
-        m, n = len(grid), len(grid[0])
-
-        dp = [[0] * n for _ in range(m)]
-        dp[0][0] = grid[0][0]
-
-        for i in range(1, m):
-            dp[i][0] = dp[i - 1][0] + grid[i][0]
-
-        for j in range(1, n):
-            dp[0][j] = dp[0][j - 1] + grid[0][j]
-
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
-
-        return dp[m - 1][n - 1]
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        if not grid or not grid[0]:
+            return 0
+            
+        m = len(grid)
+        n = len(grid[0])
+        
+        # Fill out the first row (can only come from the left)
+        for c in range(1, n):
+            grid[0][c] += grid[0][c - 1]
+            
+        # Fill out the first column (can only come from above)
+        for r in range(1, m):
+            grid[r][0] += grid[r - 1][0]
+            
+        # Fill out the rest of the grid
+        for r in range(1, m):
+            for c in range(1, n):
+                grid[r][c] += min(grid[r - 1][c], grid[r][c - 1])
+                
+        # The bottom-right corner holds the absolute minimum path sum
+        return grid[-1][-1]
